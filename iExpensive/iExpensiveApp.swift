895c74418f454176c1061052cf618e685da82722
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-02-10 08:51:03 -0300

    Initial Commit

---
+++ iExpensive/iExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensiveApp.swift
+//  iExpensive
+//
+//  Created by Bruno Oliveira on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
